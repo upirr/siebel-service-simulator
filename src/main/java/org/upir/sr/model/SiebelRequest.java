a97b54ba89532c6429a2edde90ca962fc9d7e6e2
Valeriy Lapchenok <valeriy.lapchenok>
2016-01-18 13:15:42 +0200

    request/response columns size fix
    request and response are now marked as CLOB to store larger values

--- src/main/java/org/upir/sr/model/SiebelRequest.java
+++ src/main/java/org/upir/sr/model/SiebelRequest.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.siebel.data.SiebelPropertySet;
@@ -23,10 +24,11 @@ public class SiebelRequest {
 	
 	@JsonIgnore
 	private SiebelPropertySet requestSet;
-	
+	@Lob
 	private String request;
 	@JsonIgnore
 	private SiebelPropertySet responseSet;
+	@Lob
 	private String response;
 	private Date lastUpdated;
 	private String comments;
