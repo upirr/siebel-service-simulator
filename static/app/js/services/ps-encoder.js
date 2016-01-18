c6f41b5b8b1483f9927047c3301ffeffd8e0ae6c
Valeriy Lapchenok <valeriy.lapchenok>
2016-01-18 13:16:00 +0200

    added encoding

--- static/app/js/services/ps-encoder.js
+++ static/app/js/services/ps-encoder.js
@@ -1,51 +1,87 @@
 app.service('psEncoder', function () {
 
-    //eto ebuchaja kopija govnokoda iz propertySeta JDB
-    this.encode = function (xmlString) {
-        if (!xmlString)
-            return "";
-        var xml = $($.parseXML(xmlString.replace(/(\r\n|\n|\r)/gm,""))).children()[0];
-        var write = function (value) {
-            r += value + "*";
-        }
+	this.encode = function (xmlString) {
+		if (!xmlString)
+			return "";
 
-        var writePs = function (target) {
-            
-            write(target.attributes.length);
-            write(target.children.length);
-            writeString(target.nodeName);
-			var textValue = _.find(target.childNodes, function(node) { return node.data; });
-            writeValue(textValue && textValue.data);
+		var xml = $($.parseXML(this.escape(xmlString))).children()[0];
+		var write = function (value) {
+			r += value + "*";
+		}
 
-            for (var i = 0; i < target.attributes.length; i++) {
-                writeString(target.attributes[i].name);
-                writeString(target.attributes[i].value);
-            }
-            
-            for (var i = 0; i < target.children.length; i++) {
-                writePs(target.children[i]);
-            }
-        }
+		var writePs = function (target) {
 
-        var writeString = function (value) {
-            var v = value || "";
-            write(v.length);
-            r += v;
-        }
+			write(target.attributes.length);
+			write(target.children.length);
+			writeString(target.nodeName);
+			var textValue = _.find(target.childNodes, function (node) {
+				return node.data;
+			});
+			writeValue(textValue && textValue.data);
 
-        var writeValue = function (value) { //meaning ps <Value>, tut eshe dolzhna bytj realizacija base64, no mne lenj
-            write(3);
-            writeString(value);
-        }
+			for (var i = 0; i < target.attributes.length; i++) {
+				writeString(target.attributes[i].name);
+				writeString(target.attributes[i].value);
+			}
 
-        var r = "@";
-        //header
-        write(0);
-        write(0);
+			for (var i = 0; i < target.children.length; i++) {
+				writePs(target.children[i]);
+			}
+		}
 
-        //ps
-        writePs(xml);
+		var writeString = function (value) {
+			var v = value || "";
+			write(v.length);
+			r += v;
+		}
 
-        return r;
-    }
+		var writeValue = function (value) { //meaning ps <Value>, tut eshe dolzhna bytj realizacija base64, no mne lenj
+			write(3);
+			writeString(value);
+		}
+
+		var r = "@";
+		//header
+		write(0);
+		write(0);
+
+		//ps
+		writePs(xml);
+
+		return r;
+	};
+        
+	this.escape = function (str) {
+		try {
+			str = str.replace(/(\r\n|\n|\r)/gm," ")
+			//find all attributes
+			var attrs = [];
+			var processed = "";
+			var match;
+			var re = /=\".*\"/igm;
+
+			while ((match = re.exec(str)) != null) {
+				attrs.push({
+					index: match.index,
+					value: match[0]
+				});
+			}
+			
+			var pos = 0;
+
+			//replace ampersant with &quot;
+			_.each(attrs, function (attr) {
+				processed += str.substr(pos, attr.index - pos);
+				processed += attr.value.replace(/\&(?!amp;)/gim, "&amp;");
+				pos = attr.index + attr.value.length;
+			});
+			
+			processed += str.substr(pos);
+			
+		} catch (e) {
+			throw e;
+		}
+		
+		return processed;
+	}
 });
